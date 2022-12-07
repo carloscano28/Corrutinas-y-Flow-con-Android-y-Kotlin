caf26a94e833d0fc7f45029787a752b9de0228f9
Carlos R Cano <carlos.canon@elektra.com.mx>
2022-12-06 19:04:50 -0600

    - Creacion de proyecto

---
+++ app/src/main/java/com/example/curso_completo_de_corrutinas_y_flow_con_android_y_kotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.curso_completo_de_corrutinas_y_flow_con_android_y_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/test/java/com/example/curso_completo_de_corrutinas_y_flow_con_android_y_kotlin/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.curso_completo_de_corrutinas_y_flow_con_android_y_kotlin
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
